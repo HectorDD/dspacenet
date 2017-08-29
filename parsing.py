8dfb5e3e405dc61a9af0824f1f3a2490da02c9e5
hectordavid1228 <hectordavid1228@hotmail.com>
2017-08-29 23:06:50 +0000

    Solving problems with spaces after a post and say. Adding the button for request an space (without functionality)

--- parsing.py
+++ parsing.py
@@ -1,24 +1,19 @@
 # coding=utf-8
 from parse import * 
 clock=2
-def extractInfo(msg):
-    global clock
-    parseResult=parse('<{}>{}',msg)
-    if parseResult is None :
-        if msg.find("{pid") == -1:
-            r={'clock' : clock , 'user_msg' : "private" , 'msg' : msg , 'class' : "system" }
-        else:
-            msg=msg.replace("{","[")
-            msg=msg.replace("}","]")
-            r={'clock' : clock , 'user_msg' : "private" , 'msg' : msg , 'class' : "process" }
-    else :
-        info=parseResult[0]
-        message=parseResult[1]
-        info=info.split("|")
-        r={'clock' : info[0] , 'user_msg' : info[1] , 'msg' : message , 'class' : "none" }
-    return r
+def erraseSpacePostAndSay(program,searchingString):
+    index=program.find(searchingString)
+    oldindex=0
+    while index != -1 :
+        index=index+oldindex+4
+        print index
+        while program[index] == " ":
+            program=program[:index] + program[index+1:]
+        oldindex=index
+        print("old",oldindex)
+        index=program[index:].find(searchingString)
+        print("index",index)
+    return program
 
-l=['<365|hectordavid1228>helloworld', '<366|hectordavid1228>blablabla', '<366|hectordavid1228>hello,thisismywall', "{pid:365|hectordavid1228}post('helloworld')", "{pid:366|hectordavid1228}post('blablabla')", "{pid:366|hectordavid1228}post('hello,thisismywall')"]
-
-for i in l:
-    print extractInfo(i)
+print erraseSpacePostAndSay("post       (alasjas)    post   (aslknaskjds)    post(asdsgf) post () ","post")
+        
