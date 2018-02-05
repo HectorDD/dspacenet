fd56c69d5313fef7d3d85d6bce5daccfa95d994c
hectordavid1228 <hectordavid1228@gmail.com>
2018-02-05 18:24:13 -0500

    pre-pull

--- node/helpers/tickWorker.js
+++ node/helpers/tickWorker.js
@@ -1,3 +1,3 @@
-require('./sccpClient').runSCCP('enter @ "clock" do ( say("tick") )', process.argv[2], 'admin', 0);
+require('./sccpClient').runSCCP('enter @ "clock" do signal("tick")', process.argv[2], 'admin', 0);
 
 process.stdout.write(`excuted path: ${process.argv[2]}`);
