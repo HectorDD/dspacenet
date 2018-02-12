57e73ce1287bb4004d4670bb0be872c914ca008a
KinIcy NET <nkinicy@gmail.com>
2018-02-12 10:09:13 -0500

    client moved to a new repository: https://github.com/KinIcy/dspacenet-client

--- client/middleware/resolvePath.js
+++
@@ -1,8 +0,0 @@
-export default async function (ctx) {
-  console.log('resolve-path-filter')
-  if (!ctx.params.space) return
-  if (/^(|\d+(\.\d+)*)$/.test(ctx.params.space)) {
-    const { alias } = await ctx.app.$axios.$get(`meta/space/${ctx.params.space}`)
-    ctx.redirect(`/space/${alias}`)
-  }
-}
--- client/startnuxt.sh
+++
@@ -1,2 +0,0 @@
-cd /home/dspacenet/dspacenet/client/
-PORT=80 HOST=0.0.0.0 npm start
